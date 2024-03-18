a03f78a77590a88decbbafc8fcc66d08286b0319
stark <songgao.cao@outlook.com>
2024-03-18 16:26:37 +0400

    fix service worker

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -18,30 +18,28 @@ const isLocalhost = Boolean(
     )
 );
 
-export default function register() {
-  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-    // The URL constructor is available in all browsers that support SW.
-    const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
-    if (publicUrl.origin !== window.location.origin) {
-      // Our service worker won't work if PUBLIC_URL is on a different origin
-      // from what our page is served on. This might happen if a CDN is used to
-      // serve assets; see https://github.com/facebookincubator/create-react-app/issues/2374
-      return;
-    }
-
-    window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-
-      if (isLocalhost) {
-        // This is running on localhost. Lets check if a service worker still exists or not.
-        checkValidServiceWorker(swUrl);
-      } else {
-        // Is not local host. Just register service worker
-        registerValidSW(swUrl);
+const registerServiceWorker = async () => {
+  if ("serviceWorker" in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register("/sw.js", {
+        scope: "/",
+      });
+      if (registration.installing) {
+        console.log("正在安装 Service worker");
+      } else if (registration.waiting) {
+        console.log("已安装 Service worker installed");
+      } else if (registration.active) {
+        console.log("激活 Service worker");
       }
-    });
+    } catch (error) {
+      console.error(`注册失败：${error}`);
+    }
   }
-}
+};
+
+// …
+
+registerServiceWorker();
 
 function registerValidSW(swUrl) {
   navigator.serviceWorker
@@ -106,3 +104,6 @@ export function unregister() {
     });
   }
 }
+
+
+
